74495031d106387b970303aa1c725dfad1f46fbc
TedmanNguyen <tedmanln@gmail.com>
2026-03-09 17:52:30 -0700

    adding in character & inventory classes

---
+++ final_project_src/Characters.java
@@ -0,0 +1,28 @@
+public class Characters {
+    private int characterId;
+    private String characterName;
+    private  String characterClass;
+    private int characterLevel;
+    private Inventory inventory;
+
+    public Characters (int characterId, String characterName, String characterClass){
+        this.characterId = characterId;
+        this.characterName = characterName;
+        this.characterClass = characterClass;
+    }
+    public void setName(String newName){
+        characterName = newName;
+    }
+    public void setCharacterClass(String newClass){
+        characterClass = newClass;
+    }
+    public void setCharacterLevel(int newLevel){
+        characterLevel = newLevel;
+    }
+    public int getId(){
+        return characterId;
+    }
+    public void getCharacterInfo(){
+        System.out.println("Character info: \n" + characterName + "\n" + characterClass + "\n" + characterLevel);
+    }
+}
