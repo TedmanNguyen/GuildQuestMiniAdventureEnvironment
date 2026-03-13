40c09e38f9fe588520edeb7b3caa14a1c784327f
joshngn <joshn0814@gmail.com>
2026-03-12 21:49:33 -0700

    started miniAdventure class

--- final_project_src/MiniAdventure.java
+++ final_project_src/MiniAdventure.java
@@ -1,4 +1,20 @@
 public abstract class MiniAdventure {
-    
-    public abstract void launch();
+
+    protected Realm realm;
+    protected Characters player1;
+    protected Characters player2;
+    protected boolean gameOver;
+
+    MiniAdventure(Realm realm, Characters player1, Characters player2 ){
+        this.realm = realm;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.gameOver = false;
+    }
+
+    public abstract void launch(
+
+    );
+
+
 }
